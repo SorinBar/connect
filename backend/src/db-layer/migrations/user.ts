1eb83c69da42a7c8fad6e941bfcb65acc19c629d
Sorin Barbu <sbarbu38@yahoo.com>
2023-08-06 19:22:13 +0300

    added CRUD template

--- backend/src/db-layer/migrations/user.ts
+++ backend/src/db-layer/migrations/user.ts
@@ -1,21 +1,38 @@
-import { Db } from 'mongodb';
-import { NewUserDb } from '../models/user';
+import { NewUserDb, UserDb } from '../models/user';
 import { Database, DbCollections } from '../database';
+import { createUser, deleteUser, readUser, updateUser } from '../services/user';
+import { ObjectId } from 'mongodb';
 
 export default async function usersMigrate(): Promise<void> {
-    await Database.dropCollection(DbCollections.Users);
+    console.log(`Collection: ${DbCollections.Users}`);
+
+    // await Database.dropCollection(DbCollections.Users);
 
     const usersCollection = Database.getCollection(DbCollections.Users);
-    await usersCollection.insertMany([
-        {
-            name: 'User1',
-            email: 'mail1@gmail.com',
-            password: 'pass1',
-        } as NewUserDb,
-        {
-            name: 'User2',
-            email: 'mail2@gmail.com',
-            password: 'pass2',
-        } as NewUserDb,
-    ]);
+
+    await createUser({
+        name: 'User1',
+        email: 'mail1@gmail.com',
+        password: 'pass1',
+    } as NewUserDb);
+
+    await createUser({
+        name: 'User2',
+        email: 'mail2@gmail.com',
+        password: 'pass2',
+    } as NewUserDb);
+
+    const user = await readUser(new ObjectId('64cfc1808b50a1dceff1973c'));
+    console.log(user);
+
+    await updateUser({
+        _id: new ObjectId('64cfc1808b50a1dceff1973c'),
+        name: 'X',
+        email: 'modified@gmail.com',
+        password: 'modified',
+    } as UserDb);
+
+    await deleteUser(new ObjectId('64cfb576af7c8ea25db2c891'));
+
+    console.log(`Done ${DbCollections.Users}`);
 }
